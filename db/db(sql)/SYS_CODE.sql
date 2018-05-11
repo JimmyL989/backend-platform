795803d7ec98b229cd3fbaf1bd22924d9252dcf2
Li, Yang <yang.li05@sap.com>
2018-05-11 16:31:14 +0800

    db init

---
+++ db/db(sql)/SYS_CODE.sql
@@ -0,0 +1,6 @@
+INSERT INTO `SYS_CODE` (`type`, `code`, `name`) VALUES ('isvalidMap', '0', '失效');
+INSERT INTO `SYS_CODE` (`type`, `code`, `name`) VALUES ('isvalidMap', '1', '有效');
+INSERT INTO `SYS_CODE` (`type`, `code`, `name`) VALUES ('logonLogMap', '1', '登录成功');
+INSERT INTO `SYS_CODE` (`type`, `code`, `name`) VALUES ('logonLogMap', '0', '登录失败');
+INSERT INTO `SYS_CODE` (`type`, `code`, `name`) VALUES ('sqlLogMap', '01', '成功');
+INSERT INTO `SYS_CODE` (`type`, `code`, `name`) VALUES ('type', '', '');
